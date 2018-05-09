adf58336ecc86eec97ecc8c08df457fa2df80d4f
Carlos <carloshin98@gmail.com>
2018-05-09 21:21:39 +0200

    Primer commit

---
+++ prueba.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+#serie = pd.Series(data,index=index)
+"""
+asignaturas = ['CSD','EDA','Redes II']
+notas = [10,9,6]
+#listaNotas= pd.Series(asignaturas, index = notas)
+listaNotas= pd.Series(asignaturas)
+"""
+notis = {"CSD":10 , "EDA":9 , "Redes II":6 , "Deontologia":7}
+
+listaNotas = pd.Series(notis)
+
+
+"""
+dic = {"asignatura":["EDA","Redes II", "CSD","IPC"] , "Dificultad":["Media","Dificil","Facil","JEJEJE"] }
+columnas= ["asignatura", "Difiultad"]
+index = [0,1,2,3];
+listaNotas = pd.Series(dic)
+"""
+print("Notas: ",end="\n")
+print(listaNotas)
